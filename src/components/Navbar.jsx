dc49ae715a88d5623af45ff62c66ddb903c1cb4f
Zoyaqureshi-coder <133286634+Zoyaqureshi-coder@users.noreply.github.com>
2025-01-21 10:59:55 +0530

    update wth navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
                 >
                     <img src={trace1} alt='trace1' className='w-9 h-9 object-contain' />
                     <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-                        Zoya &nbsp;
+                        Zoya  Qureshi &nbsp;
                         <span className='sm:block hidden'> | Front End Developer</span>
                     </p>
                 </Link>
